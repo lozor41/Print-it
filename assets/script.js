59e8ed00b4f1793fd250727f00d118e714ccd7f9
Lozor41 <pierre-8992@outlook.fr>
2023-09-26 10:44:42 +0200

    IW-20 IW-51 update dots/arrow

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+  {
+    "image": "slide1.jpg",
+    "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+  },
+  {
+    "image": "slide2.jpg",
+    "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+  },
+  {
+    "image": "slide3.jpg",
+    "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+  },
+  {
+    "image": "slide4.png",
+    "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+  }
 ]
 
 const left = document.querySelector('.arrow_left')
@@ -23,8 +23,8 @@ const imgBanner = document.querySelector('nameBanner')
 const nameBanner = document.querySelector('.tagLine')
 const dots = document.querySelector('.dots')
 
-let numberSlide = slides.length -1;
-let srcImage = "./assets/images/slideshow/";
+let numberSlide = slides.length - 1;
+let srcImage = "../assets/images/slideshow";
 let numberDot = 0;
 
 const dotsList = document.querySelectorAll(".dots")
@@ -32,41 +32,51 @@ const dotsList = document.querySelectorAll(".dots")
 console.log(numberSlide)
 
 for (let pas = 0; pas <= numberSlide; pas++) {
-    dots.innerHTML +=
-      '<span id="dot' +
-      pas +
-      '" class="dot" title="Image ' +
-      (pas + 1) +
-      '"></span>';
-  }
+  dots.innerHTML +=
+    '<span id="dot' +
+    pas +
+    '" class="dot" title="Image ' +
+    (pas + 1) +
+    '"></span>';
+}
 
 const addSelected = () => {
-    for (let pas = 0; pas <= numberSlide; pas++) {
-      if (pas === numberDot) {
-        dotsList[pas].classList.add("dot_selected");
-      } else {
-        dotsList[pas].classList.remove("dot_selected");
-      }
+  for (let pas = 0; pas <= numberSlide; pas++) {
+    if (pas === numberDot) {
+      dotsList[pas].classList.add("dot_selected");
+    } else {
+      dotsList[pas].classList.remove("dot_selected");
     }
-  };
+  }
+};
 
-for(const slide of slides)
-console.log(slide)
+console.log(srcImage)
+
+for (const slide of slides)
+  console.log(slide)
 
 const slide = {
-	prop: slides,
-	func:function() {
-		return this.prop;
-	},
+  prop: slides,
+  func: function () {
+    return this.prop;
+  },
 };
 
 console.log(slide.func())
 
 
 left.addEventListener("click", () => {
-    if (numberDot <= 0) {
-      numberDot = numberSlide;
-    } else {
-      numberDot--;
-    }
-  });
+  if (numberDot <= 0) {
+    numberDot = numberSlide;
+  } else {
+    numberDot--;
+  }
+});
+
+right.addEventListener("click", () => {
+  if (numberDot >= numberSlide) {
+    numberDot = 0;
+  } else {
+    numberDot++;
+  }
+});
